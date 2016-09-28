3fe6771f89dd57f3bab614c253c18741b1e9b4a1
tyswell <deksentys@gmail.com>
2016-09-28 14:30:18 +0700

    test

--- src/main/java/com/tagtrade/App.java
+++ src/main/java/com/tagtrade/App.java
@@ -14,7 +14,7 @@ public class App extends SpringBootServletInitializer
 {
 	
 	private static final Logger logger = LoggerFactory.getLogger(App.class);
-
+	/* TEST */
     public static void main( String[] args )
     {    	
     	SpringApplication.run(App.class, args);
