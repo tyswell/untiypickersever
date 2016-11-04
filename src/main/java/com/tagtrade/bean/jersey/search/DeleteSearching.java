47cb7fefe83858ebc4d5b2038daa31904c8583b8
tyswell <deksentys@gmail.com>
2016-11-04 15:13:51 +0700

    delete service

---
+++ src/main/java/com/tagtrade/bean/jersey/search/DeleteSearching.java
@@ -0,0 +1,25 @@
+package com.tagtrade.bean.jersey.search;
+
+public class DeleteSearching {
+	
+	private String tokenId;
+
+	private Integer searchingId;
+
+	public String getTokenId() {
+		return tokenId;
+	}
+
+	public void setTokenId(String tokenId) {
+		this.tokenId = tokenId;
+	}
+
+	public Integer getSearchingId() {
+		return searchingId;
+	}
+
+	public void setSearchingId(Integer searchingId) {
+		this.searchingId = searchingId;
+	}
+
+}
